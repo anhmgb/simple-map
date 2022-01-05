d1cbf1d700d713fa92efee971a1dd003aefa83ce
bao-anh <mgbanh97@gmail.com>
2022-01-05 17:04:52 +0700

    feat: integrate ant design to our app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         React
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
